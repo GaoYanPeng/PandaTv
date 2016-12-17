d0031df79ef6201518c12a1b457afa9a3dd3a45d
高研鹏 <18842604977@163.com>
2016-12-17 15:06:57 +0800

    基本框架

---
+++ PandaTv/app/src/main/java/com/gaoyanpeng/pandatv/base/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.gaoyanpeng.pandatv.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by dllo on 16/8/2.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayout());
+        initView();
+        initData();
+    }
+
+    //获取布局
+    protected abstract int getLayout();
+
+    //绑定控件
+    public abstract void initView();
+
+    //初始化数据
+    protected abstract void initData();
+
+    //findViewById方法
+    public <T extends View>T findView(int id){
+        return (T) findViewById(id);
+    }
+}
---
+++ PandaTv/app/src/main/java/com/gaoyanpeng/pandatv/live/LiveFm.java
@@ -0,0 +1,25 @@
+package com.gaoyanpeng.pandatv.live;
+
+import com.gaoyanpeng.pandatv.R;
+import com.gaoyanpeng.pandatv.base.BaseFragment;
+
+/**娱乐
+ * Created by 高研鹏 on 2016/12/17.
+ */
+
+public class LiveFm extends BaseFragment{
+    @Override
+    protected int initLayout() {
+        return R.layout.fm_live;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
